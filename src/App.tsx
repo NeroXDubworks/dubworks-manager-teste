849eeb89cd3baed7cfefb0e18b635987f0c0d732
NeroXDubworks <Lorrane.eugenio@outlook.com>
2026-05-07 15:41:28 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -499,8 +499,7 @@ export default function App() {
     iniciar();
 
     const { data: listener } = supabase.auth.onAuthStateChange(
-      async (_event, session) => {
-        const email = session?.user?.email;
+async (_event: string, session: any) => {        const email = session?.user?.email;
         if (!email) {
           setUsuarioLogado(null);
           return;
