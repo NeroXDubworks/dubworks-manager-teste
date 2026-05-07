4550649b051d9752e5888e39d99e46f4fc8147be
NeroXDubworks <Lorrane.eugenio@outlook.com>
2026-05-07 18:16:39 -0300

    fix supabase import

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,12 @@
+// @ts-ignore
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://omgjbafqukpzdhhpdlaa.supabase.co";
-const supabaseAnonKey = "sb_publishable_3bAOHbPjpV5RMnqb-cJKRA_cB1okqvT";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseAnonKey =
+  "sb_publishable_3bAOHbPjpV5RMnqb-cJKRA_cB1okqvT";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
